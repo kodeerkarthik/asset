b1306495d1051e66b8a73854ee72e0a2166f0148
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-05-03 14:47:14 +0530

    Desktop page completed with UI design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-			def configure_permitted_parameters
-				devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :mobile, :email, :password)}
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :mobile, :email, :password)}
 
-				devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :mobile, :password, :current_password)}
-			end
+			devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :mobile, :password, :current_password)}
+		end
+
+		def after_sign_in_path_for(resource)
+			stored_location_for(resource) || dashboard_index_path
+		end
 end
