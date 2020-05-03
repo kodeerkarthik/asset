98ef15448a94004b2df31f3cad7c6ecd8b01e927
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-05-03 22:06:16 +0530

    validation completed

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,2 +1,3 @@
 class Laptop < ApplicationRecord
+    validates :emp_id, :asset_num, :name, :MAC_address, :charger_num, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get 'laptops/index'
-  get 'laptop/index'
-  get 'desktop/index'
   get 'dashboard/index'
   devise_for :users
-  # get 'welcome/index'
   get '/' => 'welcome#index' , as: 'home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :desktops
   resources :laptops
 end
